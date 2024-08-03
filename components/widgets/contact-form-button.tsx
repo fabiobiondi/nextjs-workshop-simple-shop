c088e0710216c13184062835d9810947d7ba3a6a
Fabio Biondi <info@fabiobiondi.com>
2024-08-03 02:41:01 +0200

    remove unused import

--- components/widgets/contact-form-button.tsx
+++ components/widgets/contact-form-button.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import Loader from '@/components/utilities/loader';
-import { useFormStatus, useFormState} from "react-dom";
+import { useFormStatus } from "react-dom";
 
 interface NewsletterButton {
   sent: boolean
