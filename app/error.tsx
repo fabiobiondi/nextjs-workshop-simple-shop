b684341be6b1e26290a50865614d70c1b9167056
Fabio Biondi <info@fabiobiondi.com>
2024-08-03 02:41:20 +0200

    feat: add image to error component

--- app/error.tsx
+++ app/error.tsx
@@ -1,15 +1,18 @@
-// TODO: LAYOUT
-
 'use client'
-
+import Image from 'next/image';
 import Link from 'next/link';
 
 export default function Error({ error, reset }: { error: Error, reset: () => void }) {
   return (
-    <div>
-      <h2>Something went wrong!</h2>
-      <Link href="/about">Go to About</Link> -
-      <button onClick={() => reset()}>Try again</button>
+    <div
+      className="main-page text-center flex flex-col justify-center items-center h-full gap-4"
+    >
+      <Image src="/logo-sad.png" alt="logo sad" width={300} height={300}/>
+      <h1 className="widget-title">Something went wrong!</h1>
+
+      <button className="btn btn-secondary" onClick={() => reset()}>Try again</button>
+      <Link href="/" className="btn btn-secondary">Back to Home</Link>
+
     </div>
   )
 }
